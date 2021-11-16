663a90a7d35d795631c347ede44a0162159228b5
Dean Abbott <deanabbott293@gmail.com>
2021-11-16 12:03:31 +0900

    place address validation test written

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,4 @@
 class Place < ApplicationRecord
 validates :name, presence:true, length: { minimum: 1 }
+validates :address, presence:true, length: { minimum: 10}
 end
--- spec/models/places_spec.rb
+++ spec/models/places_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Place, type: :model do
-  it 'has a name' do
+  it 'has a name of more than at least 1 character' do
     place = Place.new(
       name: '',
       address: '',
@@ -11,5 +11,17 @@ RSpec.describe Place, type: :model do
     expect(place).to_not be_valid
     place.name = "Q"
     expect(place).to be_valid
+  end
+
+  it 'has an address' do
+    place = Place.new(
+      name: 'Q',
+      address: '',
+      latitude: 0,
+      longitude: 0,
+    )
+    expect(place).to_not be_valid
+    # place.address = "16 kamakura crescent, Kamakura"
+    # expect(place).to be_valid
   end
 end
