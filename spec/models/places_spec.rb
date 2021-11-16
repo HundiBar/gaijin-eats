c38531428e464b30711701d01fe1c73634040301
Dean Abbott <deanabbott293@gmail.com>
2021-11-16 11:40:50 +0900

    place model test created

---
+++ spec/models/places_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Place, type: :model do
+  it 'has a name' do
+    place = Place.new(
+      name: '',
+      address: '',
+      latitude: 0,
+      longitude: 0,
+    )
+    expect(place).to_not be_valid
+  end
+
+end
