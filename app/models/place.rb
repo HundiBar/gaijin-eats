397ef5ea799d496202de1b0d9a75710b6feb7a26
Dean Abbott <deanabbott293@gmail.com>
2021-11-16 12:59:11 +0900

    geocoder gem setup

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,8 @@
 class Place < ApplicationRecord
-geocoded_by :address
-after_validation :geocode
-validates :name, presence:true, length: { minimum: 1 }
-validates :address, presence:true, length: { minimum: 10}
-validates :latitude, numericality: true, presence:true
-validates :longitude, numericality: true, presence:true
+  geocoded_by :address
+  after_validation :geocode
+  validates :name, presence:true, length: { minimum: 1 }
+  validates :address, presence:true, length: { minimum: 10}
+  validates :latitude, numericality: true, presence:true
+  validates :longitude, numericality: true, presence:true
 end
