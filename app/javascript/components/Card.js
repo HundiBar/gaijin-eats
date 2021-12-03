5ba28781db3a6d261fdc6fb25994dee05cc3f724
Dean Abbott <deanabbott293@gmail.com>
2021-12-03 14:19:41 +0900

    card names generated from db as react componnents

--- app/javascript/components/Card.js
+++ app/javascript/components/Card.js
@@ -3,8 +3,9 @@ import React from 'react'
 export default function Card(props) {
   console.log(props)
   return (
-    <div>
-      <h1><a href={`https://www.google.com/maps/place/${props.name}/${props.address}`} target="_blank">{props.name}</a></h1>
+    <div className="card">
+      <h1 className="card--title"><a href={`https://www.google.com/maps/place/${props.name}/${props.address}`} target="_blank">{props.name}</a></h1>
+      <p className="card--address">{props.address}</p>
     </div>
   )
 }
