da880b56d172540a913d01298aa12e0fae627081
Dean Abbott <deanabbott293@gmail.com>
2021-12-03 13:31:29 +0900

    maps margin

--- app/javascript/components/Map.js
+++ app/javascript/components/Map.js
@@ -22,46 +22,48 @@ const Map = (props) => {
   };
 
   return (
-    <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={position}
-      zoom={props.zoom}
-    >
-      <MarkerClusterer
-        minimumClusterSize='10'
+    <div className="map-wrapper">
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={position}
+        zoom={props.zoom}
       >
-        {(clusterer) =>
-          props.markers[0].map(marker =>
-            <Marker
-              key={marker.id}
-              animation={2}
-              position={{
-                lat: marker.lat,
-                lng: marker.lng,
-              }}
-              clusterer={clusterer}
-              onClick={() => {
-                setSelected(marker)
-              }}
-            />
-          )
-        }
-      </MarkerClusterer>
-
-      {selected ?
-        (<InfoWindow
-          position={{ lat: selected.lat, lng: selected.lng }}
-          onCloseClick={() => {
-            setSelected(null)
-          }}
+        <MarkerClusterer
+          minimumClusterSize='10'
         >
-          <div style={{ maxWidth: 120 }}>
-            <img alt="no image in db as of yet"/>
-            <a href={`https://www.google.com/maps/place/${selected.address}`} target="_blank"><p>{selected.name}</p></a>
-            <small>{selected.address}</small>
-          </div>
-        </InfoWindow>) : null}
-    </GoogleMap>
+          {(clusterer) =>
+            props.markers[0].map(marker =>
+              <Marker
+                key={marker.id}
+                animation={2}
+                position={{
+                  lat: marker.lat,
+                  lng: marker.lng,
+                }}
+                clusterer={clusterer}
+                onClick={() => {
+                  setSelected(marker)
+                }}
+              />
+            )
+          }
+        </MarkerClusterer>
+
+        {selected ?
+          (<InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null)
+            }}
+          >
+            <div style={{ maxWidth: 120 }}>
+              <img alt="no image in db as of yet"/>
+              <a href={`https://www.google.com/maps/place/${selected.address}`} target="_blank"><p>{selected.name}</p></a>
+              <small>{selected.address}</small>
+            </div>
+          </InfoWindow>) : null}
+      </GoogleMap>
+    </div>
   )
 }
 
