a8d428d0d7fda355675cd8d5feea44cc271a9077
Dean Abbott <deanabbott293@gmail.com>
2021-12-09 22:33:29 +0900

    google api logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,38 +1,45 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'net/http'
+require 'json'
 
-Place.destroy_all
-puts "Creating seeds"
+url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=35.6765469,139.6752138&key=#{ENV['GMAPS_API_BROWSER_KEY']}"
+uri = URI(url)
+response = Net::HTTP.get(uri)
+data = JSON.parse(response)
+p data
+# # https://www.google.com/maps/place/Toyoda+Cheesesteak/@35.6765469,139.6752138,15z/data=!4m5!3m4!1s0x0:0xd743de5aafbdf25f!8m2!3d35.6765521!4d139.6752074
 
-Place.create!(
-  address: '〒151-0072 東京都渋谷区幡ケ谷１丁目４−1 花ビル 1F',
-  name: 'Toyoda Cheesesteak'
-)
+# # https://maps.googleapis.com/maps/api/geocode/json?latlng=35.6765469,139.6752138&key=YOUR_API_KEY
 
-Place.create!(
-  address: '〒150-0013 東京都渋谷区恵比寿１丁目２５−3 レジデンサプリマベェラァ201',
-  name: '440 Broadway Taco Shop'
-)
 
-Place.create!(
-  address: '〒142-0053 東京都品川区中延５丁目１３−16 パープルビル 2F',
-  name: 'Shango Cuban'
-)
 
-Place.create!(
-  address: '〒102-0074 東京都千代田区九段南３丁目５−4',
-  name: 'Swan & Lion'
-)
 
-Place.create!(
-  address: '〒169-0073 東京都新宿区百人町１丁目２４−8 2F A1 Shinjuku Town Plaza',
-  name: 'Mash Bros'
-)
+# Place.destroy_all
+# puts "Creating seeds"
 
-puts "Seeds compiled"
-puts "#{Place.all.count} places added"
+# Place.create!(
+#   address: '〒151-0072 東京都渋谷区幡ケ谷１丁目４−1 花ビル 1F',
+#   name: 'Toyoda Cheesesteak'
+# )
+
+# Place.create!(
+#   address: '〒150-0013 東京都渋谷区恵比寿１丁目２５−3 レジデンサプリマベェラァ201',
+#   name: '440 Broadway Taco Shop'
+# )
+
+# Place.create!(
+#   address: '〒142-0053 東京都品川区中延５丁目１３−16 パープルビル 2F',
+#   name: 'Shango Cuban'
+# )
+
+# Place.create!(
+#   address: '〒102-0074 東京都千代田区九段南３丁目５−4',
+#   name: 'Swan & Lion'
+# )
+
+# Place.create!(
+#   address: '〒169-0073 東京都新宿区百人町１丁目２４−8 2F A1 Shinjuku Town Plaza',
+#   name: 'Mash Bros'
+# )
+
+# puts "Seeds compiled"
+# puts "#{Place.all.count} places added"
