a5ad12605ea729d96a05a707b1d6b921f442188d
Dean Abbott <deanabbott293@gmail.com>
2021-12-11 16:18:26 +0900

    syntax for seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_11_065036) do
+ActiveRecord::Schema.define(version: 2021_12_11_071804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2021_12_11_065036) do
     t.string "rating"
     t.string "photo_url"
     t.string "type"
+    t.string "url"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -36,6 +36,7 @@ Place.destroy_all
 puts "Creating seeds"
 
 restaurant_urls.each do |restaurant_url|
+  place_data[:url] = restaurant_url
   place_data = fetch_place(restaurant_url)
   Place.create!(place_data)
   puts "#{place_data[:name]} created"
