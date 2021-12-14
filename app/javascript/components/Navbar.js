3387b687fe6d9f0164df47fab75abaf50e06849b
Dean Abbott <deanabbott293@gmail.com>
2021-12-14 11:11:45 +0900

    styling navbar

--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 
 export default function Navbar() {
   return (
     <nav className="nav-wrapper">
-      <h1>Gaijin Eats!</h1>
+      <div className="nav--title">
+        <h1 className="nav--main-title">Gaijin <span id="eats-title-light-font">Eats</span></h1>
+        <p className="nav--sub-title">For when you miss that home flavor</p>
+
+      </div>
     </nav>
   )
 }
