626ca47c965caa29a2f4a87a1a408bcfac55d7eb
Dean Abbott <deanabbott293@gmail.com>
2021-12-15 13:32:24 +0900

    grid setup

--- app/javascript/components/Card.js
+++ app/javascript/components/Card.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Card =({name,photo_url,url,cuisine,supermarket}) => {
   return(
-    <div className="card">
+    <div className="restaurant-card">
       <img src={photo_url} alt="no"/>
-      <p>{name}</p>
-      <p>{cuisine}</p>
+      <p className="restaurant-card--name">{name}</p>
+      <p className="restaurant-card--cuisine">{cuisine}</p>
     </div>
 
   )
--- app/javascript/components/CardsList.js
+++ app/javascript/components/CardsList.js
@@ -34,9 +34,24 @@ const cardsList = ({places}) => {
   })
   return (
     <div className="card-wrapper">
-      {cardsRenderRestaurants}
-      <h2> Whadddup bitches!</h2>
-      {cardsRenderSupermarkets}
+      <div className="restaurants">
+        <div>
+          <h2 className="cards--title">Cafes &amp; Restaurants</h2>
+          <p className="cards--subtitle">Find that homey flavor you’ve been craving from back home</p>
+        </div>
+        <div className= "cards">
+          {cardsRenderRestaurants}
+        </div>
+      </div>
+      <div className="supermarkets">
+        <div>
+          <h2 className="cards--title">Supermarkets</h2>
+          <p className="cards--subtitle">The places that can make your home feel like home again</p>
+        </div>
+        <div className="cards">
+          {cardsRenderSupermarkets}
+        </div>
+      </div>
     </div>
   )
 }
