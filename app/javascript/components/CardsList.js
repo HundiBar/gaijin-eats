a414283181f04e15bc8ab0571869b75515996e81
Dean Abbott <deanabbott293@gmail.com>
2021-12-21 15:53:44 +0900

    notes for comnbining branches

--- app/javascript/components/CardsList.js
+++ app/javascript/components/CardsList.js
@@ -62,15 +62,12 @@ const cardsList = ({ places }) => {
           ?
           <p className="not-found-message">Sorry, we couldn't any find places from this category</p>
           : <div className="cards">{cardsRenderRestaurants}</div>}
-
-         { visibleRestaurants < restaurants.length ? <LoadMoreButton loadMore={loadMoreRestaurants}></LoadMoreButton> : <div className="no-button-style"></div>}
+          { visibleRestaurants < restaurants.length ? <LoadMoreButton loadMore={loadMoreRestaurants}></LoadMoreButton> : <div className="no-button-style"></div>}
 
       </div>
       <div className="card">
         <h2 className="cards--title">Supermarkets</h2>
         <p className="cards--subtitle">The places that can make your home feel like home again</p>
-
-
         {cardsRenderSupermarkets.length === 0
           ?
           <p className="not-found-message">Sorry, we couldn't find any places from this category</p>
