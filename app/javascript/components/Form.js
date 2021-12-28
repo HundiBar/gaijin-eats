e5f99e4d5788aaaa625d8cbbd46bca1487ee5d30
Dean Abbott <deanabbott293@gmail.com>
2021-12-28 16:46:08 +0900

    new page input form

--- app/javascript/components/Form.js
+++ app/javascript/components/Form.js
@@ -4,33 +4,33 @@ export default function Form() {
   return (
     <>
       <div className="form--wrapper">
-        <h1>Submit a location</h1>
-        <p>Once submitted, it may take a few days for your location to show up.</p>
+        <h1 className="form--title">Submit a location</h1>
+        <p className="form--subtitle">Once submitted, it may take a few days for your location to show up.</p>
         <form action="https://getform.io/f/776f5d76-8ecc-4234-bb5b-9d7401f496ec" method="POST">
-          <div class="form-group">
-            <label id="location" for="location">Location name</label>
-            <input type="text" name="location" class="form-control" id="location"/>
+          <div className="form-group">
+            <label className="input-title required" id="location" for="location">Location name</label>
+            <input type="text" name="location" class="form-control" id="location" required/>
           </div>
-          <div class="form-group">
-            <label id="googlelink" for="googlelink">Google map link</label>
-            <input type="text" name="googlelink" class="form-control" id="googlelink" />
+          <div className="form-group">
+            <label className="input-title required" id="googlelink" for="googlelink">Google map link</label>
+            <input type="text" name="googlelink" className="form-control" id="googlelink" required />
           </div>
-          <div class="form-group">
-            <label id="googleimagelink" for="googleimagelink">Google image link</label>
-            <label for="googleimagelink">Image that best represents the place</label>
-            <input type="text" name="googleimagelink" class="form-control" id="googleimagelink" />
+          <div className="form-group">
+            <label className="input-title required" id="googleimagelink" for="googleimagelink">Google image link</label>
+            <label className="input-subtitle" for="googleimagelink">Image that best represents the place</label>
+            <input type="text" name="googleimagelink" className="form-control" id="googleimagelink" required/>
           </div>
-          <div class="form-group">
-            <label id="placetags" for="placetags">Place tags</label>
-            <label for="placetags">Ex: American; International. Add tags separated by commas.</label>
-            <input type="text" name="placetags" class="form-control" id="placetags" />
+          <div className="form-group">
+            <label className="input-title required" id="placetags" for="placetags">Place tags</label>
+            <label className="input-subtitle" for="placetags">Ex: American; International. Add tags separated by commas.</label>
+            <input type="text" name="placetags" className="form-control" id="placetags" required/>
           </div>
-          <div class="form-group">
-            <label id="contactinfo" for="exampleFormControlInput1">Your contact information</label>
-            <label for="contactinfo">Email address and/or name</label>
-            <input type="text" name="contactinfo" class="form-control" id="contactinfo"/>
+          <div className="form-group">
+            <label className="input-title" id="contactinfo" for="exampleFormControlInput1">Your contact information</label>
+            <label className="input-subtitle" for="contactinfo">Email address and/or name</label>
+            <input type="text" name="contactinfo" className="form-control" id="contactinfo"/>
           </div>
-          <button type="submit" class="website-button">Submit</button>
+          <button type="submit" className="submit-button">Submit</button>
         </form>
       </div>
     </>
