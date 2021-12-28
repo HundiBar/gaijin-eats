a9e9b193027c9fb8df1744acd6415eef8ec196c1
Dean Abbott <deanabbott293@gmail.com>
2021-12-28 15:31:20 +0900

    new page input form

--- app/javascript/components/Form.js
+++ app/javascript/components/Form.js
@@ -3,14 +3,28 @@ import React from "react";
 export default function Form() {
   return (
     <div>
+      <h1>Submit a location</h1>
+      <p>Once submitted, it may take a few days for your location to show up.</p>
       <form action="https://getform.io/f/776f5d76-8ecc-4234-bb5b-9d7401f496ec" method="POST">
         <div class="form-group">
-          <label id="emailinput" for="exampleFormControlInput1">EMAIL ADDRESS</label>
-          <input type="email" name="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+          <label id="location" for="location">Location name</label>
+          <input type="text" name="location" class="form-control" id="location"/>
         </div>
         <div class="form-group">
-          <label id="messageinput" for="exampleFormControlTextarea1">MESSAGE</label>
-          <textarea class="form-control" name="message" id="exampleFormControlTextarea1" rows="5"></textarea>
+          <label id="googlelink" for="googlelink">Google map link</label>
+          <input type="text" name="googlelink" class="form-control" id="googlelink" />
+        </div>
+        <div class="form-group">
+          <label id="googleimagelink" for="googleimagelink">Google image link</label>
+          <input type="text" name="googleimagelink" class="form-control" id="googleimagelink" />
+        </div>
+        <div class="form-group">
+          <label id="placetags" for="placetags">Place tags</label>
+          <input type="text" name="placetags" class="form-control" id="placetags" />
+        </div>
+        <div class="form-group">
+          <label id="contactinfo" for="exampleFormControlInput1">Your contact information</label>
+          <input type="text" name="contactinfo" class="form-control" id="contactinfo"/>
         </div>
         <button type="submit" class="website-button">Submit</button>
       </form>
