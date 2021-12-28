1358f505453550037069d40059446a0dab7960a3
Dean Abbott <deanabbott293@gmail.com>
2021-12-28 17:55:15 +0900

    conditional button

--- app/javascript/components/Button.js
+++ app/javascript/components/Button.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
 const button = () => {
-  return (
-    <div>
-      <button className="submit-location-button" >Submit a location</button>
-    </div>
+  const [resize, setResize] = useState(window.innerWidth)
 
+  const handleResize = () => {
+    setResize(window.innerWidth);
+  };
+
+  window.addEventListener("resize", handleResize);
+  const buttonSelect = () => {
+    if (resize <= 500) {
+      return (
+        <div>
+          <button className="submit-location-phone" >Submit</button>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <button className="submit-location-button" >Submit a location</button>
+        </div>
+      )
+    }
+  }
+  return (
+    <>
+      {buttonSelect()}
+    </>
   )
 }
 
