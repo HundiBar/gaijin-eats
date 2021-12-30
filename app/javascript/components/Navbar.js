2d09c4cd19b0c5f3c3a56345dbf1deff015bb224
Dean Abbott <deanabbott293@gmail.com>
2021-12-30 11:43:31 +0900

    secondary page link

--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <nav className="nav-wrapper">
       <div className="nav--title">
-        <a href='https://gaijin-eats.herokuapp.com/' onClick={location.reload()}>
+        <a href='https://gaijin-eats.herokuapp.com/' target="_blank">
           <h1 className="nav--main-title">Gaijin <span id="eats-title-light-font">Eats</span></h1>
         </a>
         <p className="nav--sub-title">For when you miss that home flavor</p>
