9161c7f76c4a2ecbc56a7591a012d9bda5624f89
Dean Abbott <deanabbott293@gmail.com>
2022-01-02 19:45:28 +0900

    map laod working on link

--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -4,9 +4,9 @@ export default function Navbar() {
   return (
     <nav className="nav-wrapper">
       <div className="nav--title">
-        <a href='https://gaijin-eats.herokuapp.com/' target="_blank">
+        {/* <a href='https://gaijin-eats.herokuapp.com/' target="_blank"> */}
           <h1 className="nav--main-title">Gaijin <span id="eats-title-light-font">Eats</span></h1>
-        </a>
+        {/* </a> */}
         <p className="nav--sub-title">For when you miss that home flavor</p>
 
       </div>
