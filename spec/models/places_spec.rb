2b6f3c563449d36b0772a9e34578350dfcaf459b
Dean Abbott <deanabbott293@gmail.com>
2022-01-07 12:33:03 +0900

    latitude value testing of nil to be not valid

--- spec/models/places_spec.rb
+++ spec/models/places_spec.rb
@@ -19,17 +19,16 @@ RSpec.describe Place, :type => :model do
 
   it 'is not valid without an address of minimum length of 10' do
     subject.address = "less 10"
+    subject.to_s.length
     expect(subject).to_not be_valid
   end
 
   it 'is not valid without a latitude value' do
     subject.latitude = nil
-    puts subject.latitude
-    puts subject.latitude.class
     expect(subject).to_not be_valid
   end
 
-  it 'has a longitude which is long enough minimum' do
+  it 'is not valid without a longitude value' do
     place = described_class.new(
       name: 'Q',
       address: '16 kamakura Krescent, Kamakura',
